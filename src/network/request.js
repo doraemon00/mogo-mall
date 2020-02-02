b7f827fbc32303ad413b332bd976b962f12843c6
doraemon00 <26552699@qq.com>
2020-02-02 16:44:55 +0800

    add 首页请求数据

---
+++ src/network/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export function request(config){
+    //1.创建 axios 实例
+    const instance = axios.create({
+        // baseURL:"http://123.207.32.32:8000/api/hy",
+        baseURL:"http://106.54.54.237:8000/api/hy",
+        timeout:5000
+    })
+    //2.axios 的拦截器
+    //2.1.请求拦截的作用
+    instance.interceptors.request.use(config=>{
+        return config
+    },err => {
+        return err
+    })
+
+    //2.2.拦截响应
+    instance.interceptors.response.use(res=>{
+        return res.data
+    },err => {
+        return err
+    })
+
+    //3.发送真正的网络请求
+    return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,10 +6,27 @@
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
 export default {
   name: "Home",
+  data(){
+      return{
+          banners:[],
+          recommends:[]
+      }
+  },
   components:{
       NavBar
+  },
+  created(){
+      //1.请求多个数据
+      getHomeMultidata().then(res=>{
+        //   this.result = res
+        // console.log(res)
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+    
+      })
   }
 
 };

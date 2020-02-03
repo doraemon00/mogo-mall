ced91cc812122171753069dcddf2b55a4ffcbbe9
doraemon00 <26552699@qq.com>
2020-02-03 19:25:47 +0800

    add better-scroll 的封装以及使用

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,15 +3,13 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners" />
-    <recommend-view :recommends="recommends" />
-    <feature-view />
-    <tab-control 
-    class="tab-control" 
-    :titles="['流行','新款','精选']" 
-    @tabClick="tabClick"
-     />
-    <good-list :goods="showGoods"/>
+    <scroll class="content">
+      <home-swiper :banners="banners" />
+      <recommend-view :recommends="recommends" />
+      <feature-view />
+      <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick" />
+      <good-list :goods="showGoods" />
+    </scroll>
   </div>
 </template>
 
@@ -22,7 +20,8 @@ import FeatureView from "./childComps/FeatureView";
 
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl";
-import GoodList from 'components/content/goods/GoodsList'
+import GoodList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
 
 import { getHomeMultidata, getHomeGoods } from "network/home";
 
@@ -34,7 +33,8 @@ export default {
     RecommendView,
     FeatureView,
     TabControl,
-    GoodList
+    GoodList,
+    Scroll
   },
   data() {
     return {
@@ -45,40 +45,39 @@ export default {
         new: { page: 0, list: [] },
         sell: { page: 0, list: [] }
       },
-      currentType:'pop'
+      currentType: "pop"
     };
   },
-  computed:{
-      showGoods(){
-          return this.goods[this.currentType].list
-      }
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    }
   },
 
   created() {
     //1.请求多个数据
-    this.getHomeMultidata;
+    this.getHomeMultidata();
     //2.请求商品数据
-    this.getHomeGoods('pop');
-    this.getHomeGoods('new');
-    this.getHomeGoods('sell');
-
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   methods: {
     //   事件监听相关的方法
-    tabClick(index){
-        // console.log(index)
-        switch(index){
-            case 0:
-                this.currentType = 'pop'
-                break;
-            case 1:
-                this.currentType = 'new'
-                break;
-            case 2:
-                this.currentType = 'sell'
-                break
-        }
-    },  
+    tabClick(index) {
+      // console.log(index)
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
     //   网络请求的相关方法
     getHomeMultidata() {
       getHomeMultidata().then(res => {
@@ -87,11 +86,11 @@ export default {
       });
     },
     getHomeGoods(type) {
-        const page = this.goods[type].page +1
+      const page = this.goods[type].page + 1;
       getHomeGoods(type, page).then(res => {
         console.log(res);
-        this.goods[type].list.push(...res.data.list)
-        this.goods[type].page += 1
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
       });
     }
   }
@@ -101,6 +100,8 @@ export default {
 <style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
+  position: relative;
 }
 .home-nav {
   background-color: var(--color-tint);
@@ -114,6 +115,19 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
-  z-index:9;
+  z-index: 9;
 }
+.content{
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+}
+
+
+/* .content{
+    height: calc(100%-93px);
+    margin-top: 44px;
+    
+} */
 </style>

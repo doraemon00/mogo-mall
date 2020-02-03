3271b07ed2cf0b70cdd78a62a1ba3ef1902b833c
doraemon00 <26552699@qq.com>
2020-02-03 15:06:14 +0800

    add 首页数据的请求和保存

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,8 +5,8 @@
     </nav-bar>
     <home-swiper :banners="banners" />
     <recommend-view :recommends="recommends" />
-    <feature-view/>
-    <tab-control class="tab-control" :titles="['流行','新款','精选']"/>
+    <feature-view />
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" />
   </div>
 </template>
 
@@ -16,9 +16,9 @@ import RecommendView from "./childComps/RecommendView";
 import FeatureView from "./childComps/FeatureView";
 
 import NavBar from "components/common/navbar/NavBar";
-import TabControl from 'components/content/tabControl/TabControl';
+import TabControl from "components/content/tabControl/TabControl";
 
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
@@ -32,18 +32,39 @@ export default {
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      }
     };
   },
 
   created() {
     //1.请求多个数据
-    getHomeMultidata().then(res => {
-      //   this.result = res
-      // console.log(res)
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata;
+    //2.请求商品数据
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+        const page = this.goods[type].page +1
+      getHomeGoods(type, page).then(res => {
+        console.log(res);
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      });
+    }
   }
 };
 </script>
@@ -61,8 +82,8 @@ export default {
   top: 0;
   z-index: 9;
 }
-.tab-control{
-    position: sticky;
-    top: 44px;
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>

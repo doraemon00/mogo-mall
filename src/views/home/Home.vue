eeeca57b3cb4f7fef430402492816f32399080d2
doraemon00 <26552699@qq.com>
2020-02-04 17:31:55 +0800

    fix tabControl 吸顶效果实现

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,8 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <tab-control :titles="['流行','新款','精选']" @tabClick="tabClick" 
+      ref="tabControl1" class="tab-control" v-show="isTabFixed"/>
     <scroll
       class="content"
       ref="scroll"
@@ -11,10 +13,11 @@
       :pull-up-load="true"
       @pullingUp="loadMore"
     >
-      <home-swiper :banners="banners" />
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
       <recommend-view :recommends="recommends" />
       <feature-view />
-      <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick" />
+      <tab-control :titles="['流行','新款','精选']" @tabClick="tabClick" 
+      ref="tabControl2" />
       <good-list :goods="showGoods" />
     </scroll>
     <back-top @click.native="backClick" v-show="isShowBackTop" />
@@ -57,7 +60,9 @@ export default {
         sell: { page: 0, list: [] }
       },
       currentType: "pop",
-      isShowBackTop: false
+      isShowBackTop: false,
+      tabOffsetTop:0,
+      isTabFixed:false
     };
   },
   computed: {
@@ -75,11 +80,14 @@ export default {
     this.getHomeGoods("sell");
   },
   mounted() {
+    //1.图片加载完成的事件监听
     const refresh = debounce(this.$refs.scroll.refresh, 500);
-    //3.监听 item 中图片加载完成
     this.$bus.$on("itemImageLoad", () => {
       refresh();
     });
+    //2.获取tabControl的offsetTop
+    //所有的组件都有一个属性 $el: 用户获取组件中的元素 
+    
   },
   methods: {
     //   事件监听相关的方法
@@ -96,17 +104,26 @@ export default {
           this.currentType = "sell";
           break;
       }
+      this.$refs.tabControl1.currentIndex = index
+      this.$refs.tabControl2.currentIndex = index
     },
     backClick() {
       this.$refs.scroll.scrollTo(0, 0);
     },
     contentScroll(position) {
+      //1.判断BackTop 是否显示
       this.isShowBackTop = -position.y > 1000;
       // console.log(position)
+      //2.决定 tabControl 是否吸顶（position:fixed） 
+      this.isTabFixed = (-position.y) > this.tabOffsetTop
     },
     loadMore(){
       console.log("加载更多")
       this.getHomeGoods(this.currentType)
+    },
+    swiperImageLoad(){
+      this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop
+      console.log(this.tabOffsetTop)
     },
 
     //   网络请求的相关方法
@@ -133,23 +150,19 @@ export default {
 
 <style scoped>
 #home {
-  padding-top: 44px;
+  /* padding-top: 44px; */
   height: 100vh;
   position: relative;
 }
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
-  position: fixed;
+  /* 在使用浏览器原生滚动时，为了让导航不跟随一起滚动 */
+  /* position: fixed;
   left: 0;
   right: 0;
   top: 0;
-  z-index: 9;
-}
-.tab-control {
-  position: sticky;
-  top: 44px;
-  z-index: 9;
+  z-index: 9; */
 }
 .content {
   overflow: hidden;
@@ -159,7 +172,10 @@ export default {
   left: 0;
   right: 0;
 }
-
+.tab-control{
+  position: relative;
+  z-index:9;
+}
 /* .content{
     height: calc(100%-93px);
     margin-top: 44px;

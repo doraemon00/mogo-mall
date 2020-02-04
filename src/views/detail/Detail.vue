2124976e35a6b4247a623936b96a6326495ffb80
doraemon00 <26552699@qq.com>
2020-02-04 21:28:23 +0800

    add 商品信息的展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,23 +2,27 @@
     <div id="detail">
         <detail-nav-bar></detail-nav-bar>
         <detail-swiper :top-images="topImages"></detail-swiper>
-        
+        <detail-base-info :goods="goods"/>
     </div>
 </template>
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
-import {getDetail} from "network/detail"
+import DetailBaseInfo from './childComps/DetailBaseInfo'
+
+import {getDetail,Goods} from "network/detail"
 export default {
     name:"Detail",
     components:{
         DetailNavBar,
-        DetailSwiper
+        DetailSwiper,
+        DetailBaseInfo
     },
     data(){
         return{
             iid:null,
-            topImages:[]
+            topImages:[],
+            goods:null
         }
     },
     created(){
@@ -27,8 +31,12 @@ export default {
         //2.根据iid 请求详情页数据
         getDetail(this.iid).then(res=>{
             console.log(res)
-            //获取顶部的图片轮播数据
-            this.topImages = res.result.itemInfo.topImages
+            const data = res.result
+            //1.获取顶部的图片轮播数据
+            this.topImages = data.itemInfo.topImages
+
+            //2.获取商品信息
+            this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
         })
     }
 }

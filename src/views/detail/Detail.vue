f8db9982b3e2d548079e00a9116280acbaa3d899
doraemon00 <26552699@qq.com>
2020-02-06 15:44:30 +0800

    add 点击标题跳转到对应的位置 （bug）

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="detail">
-    <detail-nav-bar class="detail-nav"/>
+    <detail-nav-bar class="detail-nav" @titleClick="titleClick" />
     <scroll class="content" ref="scroll">
+      <!-- 属性 topImage 传入值 :top-image -->
       <detail-swiper :top-images="topImages"></detail-swiper>
       <detail-base-info :goods="goods" />
       <detail-shop-info :shop="shop" />
-      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
-      <detail-param-info :param-info="paramInfo"/>
-      <detail-comment-info :comment-info="commentInfo"/>
-      <goods-list :goods="recommends"/>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+      <detail-param-info ref="params" :param-info="paramInfo" />
+      <detail-comment-info ref="comment" :comment-info="commentInfo" />
+      <goods-list ref="recommend" :goods="recommends" />
     </scroll>
   </div>
 </template>
@@ -17,16 +18,22 @@ import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
 import DetailBaseInfo from "./childComps/DetailBaseInfo";
 import DetailShopInfo from "./childComps/DetailShopInfo";
-import DetailGoodsInfo from './childComps/DetailGoodsInfo'
-import DetailParamInfo from './childComps/DetailParamInfo'
-import DetailCommentInfo from './childComps/DetailCommentInfo'
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+import DetailParamInfo from "./childComps/DetailParamInfo";
+import DetailCommentInfo from "./childComps/DetailCommentInfo";
 
 import Scroll from "components/common/scroll/Scroll";
-import GoodsList from 'components/content/goods/GoodsList'
+import GoodsList from "components/content/goods/GoodsList";
 
-import { getDetail, Goods, Shop,GoodsParam,getRecommend} from "network/detail";
-import {debounce} from 'common/utils'
-import {itemListenerMixin} from 'common/mixin'
+import {
+  getDetail,
+  Goods,
+  Shop,
+  GoodsParam,
+  getRecommend
+} from "network/detail";
+import { debounce } from "common/utils";
+import { itemListenerMixin } from "common/mixin";
 
 export default {
   name: "Detail",
@@ -41,17 +48,19 @@ export default {
     DetailCommentInfo,
     GoodsList
   },
-  mixins:[itemListenerMixin],
+  mixins: [itemListenerMixin],
   data() {
     return {
       iid: null,
       topImages: [],
       goods: {},
       shop: {},
-      detailInfo:{},
-      paramInfo:{},
-      commentInfo:{},
-      recommends:[],
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {},
+      recommends: [],
+      themeTopYs: [],
+      // getThemeTopY: null
     };
   },
   created() {
@@ -74,32 +83,73 @@ export default {
       this.shop = new Shop(data.shopInfo);
 
       //4.保存商品的详情数据
-      this.detailInfo = data.detailInfo
+      this.detailInfo = data.detailInfo;
 
       //5.获取参数信息
-    this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
-      //6. 取出评论信息 
-      if(data.rate.cRate !== 0){
-        this.commentInfo = data.rate.list[0]
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
+      //6. 取出评论信息
+      if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0];
       }
-    },
+      //第一次获取 值不对
+      //值不对的原因 this.$refs.params.$el 压根没有渲染
+      // this.$nextTick(() => {
+        // 2.第二次获取 值不对
+        // 值不对的原因 图片没有计算在内
+        //根据最新的数据 对应的Dom是已经被渲染出来
+        //但是图片依然没有加载完（目前获取到offsetTop不包含其中的图片）
+        // offsetTop值不对的时候 都是因为图片的问题
+        // this.themeTopYs = []
+        // this.themeTopYs.push(0);
+        // this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+        // this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+        // this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+        // console.log(this.themeTopYs);
+      // });
+    });
+
     //3. 请求推荐数据
-    getRecommend().then(res=>{
+    getRecommend().then(res => {
       // console.log(res)
-      this.recommends = res.data.list
-    })
-    );
+      this.recommends = res.data.list;
+    });
+
+    //4.给getThemeTopY赋值
+    // this.getThemeTopY = debounce(()=>{
+    //   this.themeTopYs = [];
+    //   this.themeTopYs.push(0);
+    //   this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+    //   this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+    //   this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+    //   console.log(this.themeTopYs);
+    // },100)
   },
-  mounted(){
+  mounted() {
     
   },
-  destroyed(){
-    this.$bus.$off('itemImageLoad',this.itemImgListenner)
+  destroyed() {
+    this.$bus.$off("itemImageLoad", this.itemImgListenner);
   },
-  methods:{
-      imageLoad(){
-          this.$refs.scroll.refresh()
-      }
+  methods: {
+    imageLoad() {
+      
+      this.$refs.scroll.refresh();
+      console.log("img1111")
+      this.getThemeTopY()
+      this.themeTopYs = [];
+      this.themeTopYs.push(0);
+      this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+      this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+      this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+      console.log(this.themeTopYs);
+    },
+    titleClick(index) {
+      console.log(index);
+      this.$refs.scroll.scrollTo(0, -this.themeTopYs[index], 200);
+    }
   }
 };
 </script>
@@ -110,12 +160,12 @@ export default {
   background-color: #fff;
   height: 100vh;
 }
-.detail-nav{
-    position: relative;
-    z-index: 9;
-    background-color: #fff;
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
 }
-.content{
-    height: calc(100% - 44px)
+.content {
+  height: calc(100% - 44px);
 }
 </style>
